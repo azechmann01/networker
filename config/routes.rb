137a447a929ea01e2da76c13c489132ded63c448
Andrea Zechmann <azechmann@chicagobooth.edu>
2017-06-05 11:55:50 -0500

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+
+  # Home Page 
+  get "/", :controller => "companies", :action => "index"
+
+  # Routes for the Company resource:
+  # CREATE
+  get "/companies/new", :controller => "companies", :action => "new"
+  post "/create_company", :controller => "companies", :action => "create"
+
+  # READ
+  get "/companies", :controller => "companies", :action => "index"
+  get "/companies/:id", :controller => "companies", :action => "show"
+
+  # UPDATE
+  get "/companies/:id/edit", :controller => "companies", :action => "edit"
+  post "/update_company/:id", :controller => "companies", :action => "update"
+
+  # DELETE
+  get "/delete_company/:id", :controller => "companies", :action => "destroy"
+  #------------------------------
+
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
